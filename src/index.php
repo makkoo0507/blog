1658322ff5880cb1ade8cf9a602a3bee9213c61a
makkoo0507 <makkoo0507@gmail.com>
2022-03-26 23:47:43 +0900

    indexページの大枠を作成,cssも適用

---
+++ src/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang=“ja”>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>MakoBlog</title>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <h1><a href="#"><img src="./img/makoBlog_Logo.png"></a></h1>
+    </header>
+    <div class="index-wrapper">
+        <div class="main-container">
+            <h2 class="title">記事一覧</h2>
+            <ul class="article-list">
+                <li class="article-item"><a href="#">記事タイトル１</a></li>
+                <li class="article-item"><a href="#">記事タイトル2</a></li>
+            </ul>
+        </div>
+    </div>
+    <footer>
+        <p>MakoBlog</p>
+    </footer>
+</body>
+
+</html>
