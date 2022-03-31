d25353c27cba7662cb8c9efcd6d9a296fd4906a6
makkoo0507 <makkoo0507@gmail.com>
2022-03-31 22:25:41 +0900

    トップページで記事一覧へ遷移するためのボタンを追加

--- src/views/top.php
+++ src/views/top.php
@@ -6,6 +6,9 @@
             <li class="article-item"><a href="#">記事タイトル１</a></li>
             <li class="article-item"><a href="#">記事タイトル2</a></li>
         </ul>
+        <div class="btn-wrapper_top">
+            <a href="#" class="btn">記事一覧へ</a>
+        </div>
     </div>
 </main>
 <div class="contact-wrap">
