1f908d4cc61fd993e5749d4c3aa9bc27fcb5392c
makkoo0507 <makkoo0507@gmail.com>
2022-03-31 20:31:54 +0900

    headerとfooter部分の共通化。

---
+++ src/views/layout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang=“ja”>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>MakoBlog</title>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <h1><a href="#"><img src="./img/makoBlog_Logo.png"></a></h1>
+</header>
+<?php include $content ?>
+<footer>
+        <p>MakoBlog</p>
+    </footer>
+</body>
+</html>
---
+++ src/views/top.php
@@ -0,0 +1,13 @@
+<div id="fv"></div>
+<main>
+    <div class="main-container">
+        <h2 class="title">最新記事5</h2>
+        <ul class="article-list">
+            <li class="article-item"><a href="#">記事タイトル１</a></li>
+            <li class="article-item"><a href="#">記事タイトル2</a></li>
+        </ul>
+    </div>
+</main>
+<div class="contact-wrap">
+    <a class="contact-btn" href="#">お問い合わせはこちら</a>
+</div>
